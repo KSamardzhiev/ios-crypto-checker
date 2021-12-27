d9f085fc3ab28fee76855155f8dc1e8b2ee60967
Kostadin Samardzhiev <k.samardzhiev@gmail.com>
2021-12-27 23:26:02 +0200

    Initial Commit

---
+++ CryptoChecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoChecker
+//
+//  Created by Kostadin Samardzhiev on 27.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
